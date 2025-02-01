8460a1e293b848920767181960f943875f6b53e4
Joel-ASantos <joel.alves.santos77@gmail.com>
2025-01-31 21:45:11 -0300

    adicionando pasta

--- Database/src/main/java/org/example/Main.java
+++ Database/src/main/java/org/example/Main.java
@@ -1,5 +1,7 @@
+package org.example;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Day one on creation database ");
+        System.out.println("Hello world!");
     }
 }
